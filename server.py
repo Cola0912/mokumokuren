ae49cf24f89f040f8bdb82feae8910311e149ce2
shusei Akiya <0912cocacola@gmail.com>
2024-05-04 01:29:43 +0900

    unko

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, render_template
 import requests
 
 app = Flask(__name__)
 
-PRINTER_URL = "http://192.168.0.14:7125"
-
 @app.route('/')
 def index():
+    # HTMLページを返す
     return render_template('index.html')
 
-@app.route('/printer/status')
+@app.route('/api/printer/status')
 def get_printer_status():
-    response = requests.get(f"{PRINTER_URL}/printer/objects/query?extruder&print_stats")
-    data = response.json()
-    return jsonify(data)
+    try:
+        response = requests.get('http://192.168.0.14:7125/printer/objects/query?heater_bed&extruder')
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data)
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
